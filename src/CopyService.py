b4a8c8451e3befe0dfe35115697a51d33b299701
el-davo <david.ahern26@gmail.com>
2017-11-25 19:27:14 +0000

    Switched to copy

--- src/CopyService.py
+++ src/CopyService.py
@@ -4,10 +4,9 @@ from FileIO import FileIO
 class CopyService:
 
     def copyFileSnapshot(self, file):
-        fileLines = FileIO().readFileLines(file.getPath())
         copyPath = os.path.join(file.getStorePath(), file.getPathHash(), file.getLastModificationTime())
         FileIO().createDirIfNotExists(copyPath)
-        FileIO().writeFileLines(os.path.join(copyPath, file.getFilename()), fileLines)
+        FileIO().copyFile(file.getPath(), copyPath)
         self.updateModicationDate(file)
 
     def updateModicationDate(self, file):
